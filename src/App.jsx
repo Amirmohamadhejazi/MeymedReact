fae582169569aa0981075fc3122a85a961f84748
Amirmohamadhejazi <Amirmohamadhejazi.9731@gmail.com>
2023-04-08 01:40:55 +0330

    fix errore

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         element: <Home/>,
     },
     {
-        path: "*",
+        path: "/MeymedReact/*",
         element: <>
             <img src={error404} alt=""/>
         </>,
